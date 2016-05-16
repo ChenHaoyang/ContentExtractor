8d41d86759a2c7cc333371533c10c23da1062c07
charles <hust.chenhaoyang@gmail.com>
2016-05-16 16:49:45 +0900

    2016/05/16

--- ContentExtractor/src/main/java/com/mad/ContentExtractor/ContentExtractor.java
+++ ContentExtractor/src/main/java/com/mad/ContentExtractor/ContentExtractor.java
@@ -1,19 +1,46 @@
 package com.mad.ContentExtractor;
 
 import java.io.*;
+import java.net.*;
 //import java.net.URL;
 import java.nio.charset.Charset;
-//import java.util.regex.Matcher;
-//import java.util.regex.Pattern;
 
+import org.mozilla.universalchardet.UniversalDetector;
+import org.apache.http.*;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.*;
+//import org.htmlcleaner.*;
 //import org.mozilla.intl.chardet.nsDetector;
 //import org.mozilla.intl.chardet.nsICharsetDetectionObserver;
 //import org.mozilla.intl.chardet.nsPSMDetector;
 
 public class ContentExtractor {
 
+	private CloseableHttpClient httpClient;
+	private RequestConfig requestConfig;
+	private UniversalDetector detector;
+	
+	public ContentExtractor(){
+		httpClient = HttpClients.custom()
+				.setRedirectStrategy(new LaxRedirectStrategy())
+				.build();
+		requestConfig = RequestConfig.custom()
+				.setSocketTimeout(3000)
+				.setConnectTimeout(2000)
+				.build();
+		detector = new UniversalDetector(null);
+	}
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		long time = System.currentTimeMillis();
@@ -23,6 +50,9 @@ public class ContentExtractor {
 		BufferedReader br = null;
 		BufferedWriter bw = null;
 		TextExtract te = new TextExtract();
+		HttpURLConnection.setFollowRedirects(true);
+		
+		//HtmlCleaner cleaner = new HtmlCleaner();
 		try {
 			br = new BufferedReader(new InputStreamReader(new FileInputStream("/home/charles/Data/input/url.csv")));
 			bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/home/charles/Data/output/chen.txt")));
@@ -39,22 +69,27 @@ public class ContentExtractor {
 			try {
 				//System.out.println(tokens[0]);
 				result = new ContentExtractor().getHTML(tokens[1].trim());
-				//System.out.println("got html");
-				String main_text = te.parse(tokens[0], result[3]);
-				bw.write("\n<document id=\"" + tokens[0] + "\" url=\"" + tokens[1] + "\">\n");
-				bw.write("<title>" + result[0] + "</title>\n");
-				bw.write("<description>" + result[1] + "</description>\n");
-				bw.write("<keywords>" + result[2] + "</keywords>\n");
-				bw.write("<main>" + main_text + "</main>\n</document>\n");
-				bw.flush();
+				//System.out.println(result[3]);
+				if(result != null){
+					String main_text = te.parse(tokens[0], result[3]).trim();
+					bw.write("\n<document id=\"" + tokens[0] + "\" url=\"" + tokens[1] + "\">\n");
+					bw.write("<title>" + result[0] + "</title>\n");
+					bw.write("<description>" + result[1] + "</description>\n");
+					bw.write("<keywords>" + result[2] + "</keywords>\n");
+					if(main_text != "")
+						bw.write("<main>" + "\n" + main_text + "\n" + "</main>\n</document>\n");
+					else
+						bw.write("<main></main>\n</document>\n");
+					bw.flush();
+				}
 				line = br.readLine();
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				line = br.readLine();
 				System.out.println(tokens[0]);
+				System.out.println(e.toString());
 				//System.out.println(e.toString());
 				continue;
-				// e.printStackTrace();
 			}
 		}
 		bw.write("</data>");
@@ -63,38 +98,166 @@ public class ContentExtractor {
 		System.out.println("Run Time: " + (System.currentTimeMillis()-time)/1000 + "s");
 	}
 
-	public String[] getHTML(String strURL) throws IOException {
+	public String[] getHTML(String strURL) throws Exception {
 		String[] result = new String[4];
-		//Entities.EscapeMode.base.getMap().clear();
-		Document doc = Jsoup.connect(strURL)
-				.timeout(5000)
-				.get();
-
-		String html = doc.outerHtml();
-		//System.out.print(html);
-		Charset old = doc.charset();
-		//System.out.println(html);
-		if(old.name() != "UTF-8")
-			html = changeCharset(html, old.name(), "UTF-8");
+		ArrayList url_info = readURL(strURL);
 		
+		if(url_info.get(0) == null) return null;
+
+		Document doc = Jsoup.parse((String)url_info.get(0));
+		
+		//check charset again
+		String detected_cs = doc.charset().name();
+		String page_cs_str = doc.select("meta[http-equiv=\"Content-Type\"]").attr("content");
+		if(!page_cs_str.equals("")){
+			String[] outter = page_cs_str.split(";");
+			if(outter.length > 1){
+				String[] inner = outter[1].split("=");
+				if(inner.length > 1){
+					detected_cs = inner[1].trim();
+				}
+			}
+		}
+		else{
+			String page_cs_str_01 = doc.select("meta").attr("charset");
+			if(!page_cs_str_01.equals("")){
+				detected_cs = page_cs_str_01.trim();
+			}
+		}
+		//if not coincident with predict charset
+		if(!detected_cs.equals(doc.charset().name())){
+			url_info.set(0, new String((byte[])url_info.get(1), detected_cs));
+			url_info.set(0, changeCharset((String)url_info.get(0), "UTF-8"));
+			doc = Jsoup.parse((String)url_info.get(0));
+		}
 		result[0] = doc.title();
 		result[1] = doc.select("meta[name=\"description\"]").attr("content");
 		result[2] = doc.select("meta[name=\"keywords\"]").attr("content");
-		result[3] = html;
+		
+		//Filtering unnecessary html tags
+		Element body = doc.select("body").first();
+		//remove link block
+		Elements link_blocks = body.select("ol:has(a),ul:has(a),div:has(a)");
+		for(Element node:link_blocks){
+			int direct_a_num = node.select(">a").size();
+			int child_of_a=0;
+			int br_node_num = node.select("br").size();
+			Elements e_a = node.select("a");
+			int a_num = e_a.size();
+			for(Element a:e_a){
+				child_of_a += (a.getAllElements().size() - 1);
+			}		
+			double child_node_num = (double)(node.getAllElements().size() - 1 - child_of_a - a_num + direct_a_num - br_node_num);
+			if( a_num / child_node_num  > 0.7 ){
+				node.remove();//drop the link block
+			}
+		}
+		
+		body.select("[id~=(?i)(header|footer|ft|links|keywords|calendar|calender|rule|attention|banner|bn|navi|recommend){1}]").remove();
+		//System.out.print(body.outerHtml());
+		body.select("[class~=(?i)(header|footer|links|calendar|calender|no_display|nodisplay|rule|attention|banner|bn|navi|month|recommend){1}]").remove();
+		//System.out.print(body.outerHtml());
+		body.select("[style~=(?i)(display[\\s]*:[\\s]*none|visible[\\s]*:[\\s]*hidden){1}]").remove();
+		//System.out.print(body.outerHtml());
+		body.select("link, select, noscript, head, header, script, style, footer, time, small, meta, h1, h2, h3, h4, h5, h6").remove();
+		body.select("iframe, textarea, input").remove();
+		//System.out.println(body.outerHtml());
+		//Document dd = Jsoup.parse("<body><t1>11<t11></t11></t1><t2><t2></t2></t2></body>");
+		//int test = dd.select("body").first().childNodeSize();
+		body.select("li:has(a), dt:has(a), dd:has(a)").remove();
+		//System.out.println(body.outerHtml());
+
+		//System.out.println(body.outerHtml());
+		//remove long text links
+		Elements noise_links = body.select("a");
+		for(Element node:noise_links){
+			if(node.html().length() >12)
+				node.remove();
+		}
+		//remove pagelink
+		body.select("a:matches(^\\d+$|前へ|次へ|戻る|トップページ|ホーム|記事|もっと見る|利用規約|案内|問い合わせ|プライバシー)").remove();
+		//remove nodes whose font-size < 10(px) | 7.5(pt) | 0.625(em)
+		Elements nodes_with_font_size = body.select("[style~=(?i)(font-size){1}]");
+		for(Element node:nodes_with_font_size){
+			String[] str = node.attr("style").toLowerCase().split(";");
+			for(String style:str){
+				if(style.contains("font-size")){
+					String[] key_val = style.split(":");
+					key_val[1] = key_val[1].trim();
+					String unit = key_val[1].substring(key_val[1].length()-2);
+					String font_size = key_val[1].substring(0, key_val[1].length()-2);
+					if(unit.equals("px")){
+						if(Integer.parseInt(font_size) < 10)
+							node.remove();
+					}
+					else if(unit.equals("pt")){
+						if(Double.parseDouble(font_size) < 7.5)
+							node.remove();
+					}
+					else if(unit.equals("em")){
+						if(Double.parseDouble(font_size) < 0.625)
+							node.remove();
+					}
+				}
+			}
+		}
+		
+		//pass the html contents after filtering
+		result[3] = body.outerHtml();
+		//System.out.println(result[3]);
 		
 		return result;
 	}
 	
-	private String changeCharset(String scr, String oldCharset, String newCharset)  throws UnsupportedEncodingException {
-		if(scr != null && newCharset != null)
+	private ArrayList readURL(String strURL) throws Exception{
+		ArrayList result = new ArrayList();
+		String html=null,encoding;
+		HttpGet httpGet = new HttpGet(strURL);
+		httpGet.setConfig(requestConfig);
+		CloseableHttpResponse response = httpClient.execute(httpGet);
+		try{
+			HttpEntity entity = response.getEntity();
+			if(entity != null){
+				InputStream in = entity.getContent();
+				ByteArrayOutputStream bao = new ByteArrayOutputStream();
+				byte[] buff = new byte[4096];
+				int bytesRead;
+
+				while((bytesRead = in.read(buff)) > 0 ){
+					if(!detector.isDone())
+						detector.handleData(buff, 0, bytesRead);
+					bao.write(buff, 0, bytesRead);
+				}
+				detector.dataEnd();
+				byte[] data = bao.toByteArray();
+				encoding = detector.getDetectedCharset();
+				if(encoding != null){
+					html = new String(data,encoding);
+					if(encoding != "UTF-8")
+						html = changeCharset(html, "UTF-8");
+				}	
+				else{
+					html = new String(data, "UTF-8");
+				}
+				result.add(html);
+				result.add(data);
+				detector.reset();
+			}
+		}
+		finally{
+			response.close();
+		}
+//		
+		return result;
+	}
+	private String changeCharset(String scr, String newCharset)  throws UnsupportedEncodingException {
+		if(scr != null)
 		{
-			byte[] bs = null;
-			if(oldCharset != null){
-				bs = scr.getBytes(newCharset);
+			if(newCharset != null){
+				return new String(scr.getBytes(newCharset), newCharset);
 			}
 			else
-				bs = scr.getBytes();
-			return new String(bs, newCharset);
+				return scr;
 		}
 		return null;
 	}
--- ContentExtractor/src/main/java/com/mad/ContentExtractor/TextExtract.java
+++ ContentExtractor/src/main/java/com/mad/ContentExtractor/TextExtract.java
@@ -19,9 +19,11 @@ public class TextExtract {
 	private static boolean flag;
 	private int start;
 	private int end, max_lines;
-	private double main_ratio, stop_ratio;
+	private double main_ratio, delay_ratio;
 	private StringBuilder text;
 	private ArrayList<Integer> indexDistribution;
+	private String m_url_id;
+	private BufferedWriter m_bw;
 	
 	public TextExtract() {
 		lines = new ArrayList<String>();
@@ -29,9 +31,14 @@ public class TextExtract {
 		text = new StringBuilder();
 		flag = true;
 		threshold	= 60;  
-		main_ratio = 0.8;
-		stop_ratio = 0.7;
+		main_ratio = 0.7;
 		max_lines = 100;
+		try {
+			m_bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/home/charles/Data/output/source/" + lines + ".txt")));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public String parse(String url_id, String _html) {
@@ -42,59 +49,58 @@ public class TextExtract {
 	public String parse(String url_id, String _html, boolean _flag) {
 		flag = _flag;
 		html = _html;
-		html = preProcess(url_id, html);
-		return getText();
+		m_url_id = url_id;
+		html = preProcess(url_id, html.replaceAll("[\b\t\r\n\f]", ""));
+		String tmp = html;
+		if(!tmp.replaceAll("[\b\t\r\n\f\\s]", "").equals(""))
+			return getText();
+		else
+			return "";
 	}
 	//private static int FREQUENT_URL = 30;
 	//private static Pattern links = Pattern.compile("<[aA]\\s+[Hh][Rr][Ee][Ff]=[\"|\']?([^>\"\' ]+)[\"|\']?\\s*[^>]*>([^>]+)</a>(\\s*.{0,"+FREQUENT_URL+"}\\s*<a\\s+href=[\"|\']?([^>\"\' ]+)[\"|\']?\\s*[^>]*>([^>]+)</[aA]>){2,100}", Pattern.DOTALL);
-	private static Pattern main_rule = Pattern.compile("(<meta.*?>|<!DOCTYPE.*?>|<!--.*?-->|<script.*?>.*?</script>|<style.*?>.*?</style>|<title.*?>.*?</title>|<select.*?>.*?</select>|<noscript.*?>.*?</noscript>|<link.*?>|<li.*?>.*?</li>|<ul.*?>.*?</ul>|<ol.*?>.*?</ol>|<dl.*?>.*?</dl>)",  Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-	private static Pattern sub_rule_01 = Pattern.compile("(<br>[\\s]*?){2}", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-	private static Pattern sub_rule_02 = Pattern.compile("(<br>|\r\n)", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-	//private static Pattern sub_rule_03 = Pattern.compile("<[^>]*['\"].*['\"].*?>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-	private static Pattern sub_rule_03 = Pattern.compile("<([^>\"]*[\"].*?[\"])+.*?>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+	private static Pattern main_rule = Pattern.compile("(<!DOCTYPE.*?>|<!--.*?-->)",//"(<meta.*?>|<!DOCTYPE.*?>|<!--.*?-->|<script.*?>.*?</script>|<style.*?>.*?</style>|"
+			//+ "<title.*?>.*?</title>|<select.*?>.*?</select>|<noscript.*?>.*?</noscript>|"
+			//+ "<link.*?>|<li.*?>.*?</li>|<ul.*?>.*?</ul>|<ol.*?>.*?</ol>|<dl.*?>.*?</dl>|"
+			//+ "<[^>]*?(display:none|visible:hidden){1}.*?>.*?</.*?>|"
+			//+ "<div[^>]*?(footer|header|links){1}.*?>(<div.*?>.*?</div>)+</div>|"
+			//+ "<div[^>]*?(footer|header|links){1}.*?>.*?</div>|"
+			//+ "<p[^>]*?(footer|header|links){1}.*?>(<p.*?>.*?</p>)+</p>|"
+			//+ "<p[^>]*?(footer|header|links){1}.*?>.*?</p>)",  
+			Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+	
+	private static Pattern sub_rule_01 = Pattern.compile("(<br[^<]*?>[ \b\t\n\f\r　]*){2}", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+	private static Pattern sub_rule_02 = Pattern.compile("(<br[^<]*?>|\r\n)", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+	//private static Pattern sub_rule_03 = Pattern.compile("<([^>\"]*[\"].*?[\"])+.*?>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
 	private static Pattern sub_rule_04 = Pattern.compile("<.*?>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+	private static Pattern sub_rule_05 = Pattern.compile("(</p>|</tr>|</li>|</dd>|</dt>)", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+	private static Pattern sub_rule_06 = Pattern.compile("(</div>|</table>|</section>|</dl>)", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+	//private static Pattern sub_rule_06 = Pattern.compile("<a[ ]+.*?>.{12,}?</a>", Pattern.DOTALL | Pattern.UNICODE_CASE);
+	//private static Pattern sub_rule_03 = Pattern.compile("<[^>]*['\"].*['\"].*?>", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+
 	
 	private static String preProcess(String url_id, String source) {
-		source = StringEscapeUtils.unescapeHtml(source);
-		//source = source.replaceAll("(?is)<!DOCTYPE.*?>", "");
-		//source = source.replaceAll("(?is)<!--.*?-->", "");				// remove html comment
-		//source = source.replaceAll("(?is)<(script|style|title|span|select|noscript).*?>.*?</(script|style|title|span|select|noscript)>", "");
-		//source = source.replaceAll("(?is)(<meta[^<]*?>|<!DOCTYPE.*?>|<!--.*?-->|<script.*?>.*?</script>|<style.*?>.*?</style>|<title.*?>.*?</title>|<select.*?>.*?</select>|<noscript.*?>.*?</noscript>|<li[^<]*?>.*?</li>|<ul.*?>.*?</ul>|<ol.*?>.*?</ol>|<dl.*?>.*?</dl>)", "");
-		//source = source.replaceAll("(?is)<script.*?>.*?</script>", "");
-		//source = source.replaceAll("(?is)<style.*?>.*?</style>", "");
-		//source = source.replaceAll("(?is)<title.*?>.*?</title>", "");
-		//source = source.replaceAll("(?is)<span.*?>.*?</span>", "");
-		//source = source.replaceAll("(?is)<select.*?>.*?</select>", "");
-		//source = source.replaceAll("(?is)<noscript.*?>.*?</noscript>", "");
-//		if(flag)
-//			source = source.replaceAll("(?is)<a.*?>.*?</a>", "");
 		//System.out.println(source);
-		//int len = source.length();
-		//source = "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://st.shinobi.jp/img/services/homepage/commercial.css\" /></HEAD>";
+		//source = "<div id=\"header_ad\"><script></script><div></div></div><div></div>";
 		source = main_rule.matcher(source).replaceAll("");
-//		while ((source = links.matcher(source).replaceAll("")).length() != len)
-//		{
-//			len = source.length();
-//		}
-			//continue;
-		
-		//source = links.matcher(source).replaceAll("");
 		//System.out.println(source);
-		
+		//source = source.replaceAll("<a [^>]*?>[^<]{11,}?</a>", "");
+		//System.out.println(source);
 		while(sub_rule_01.matcher(source).find())
 			//source = source.replaceAll("(<br>[\\s]*?){2}", "<br>");
 			source = sub_rule_01.matcher(source).replaceAll("<br>");
+		//System.out.println(source);
+		//System.out.println(source);
 		//source = source.replaceAll("(<br>|\r\n)", "\n");
 		//source = source.replaceAll("(?is)<.*?>", "");
 		source = sub_rule_02.matcher(source).replaceAll("\n");
-		//if(url_id.equals("222447375"))
-			//System.out.println("11");
-		//source = "<a id=\"_ctl0_CPH2_Pager2_HL_PreImg\" href=\"/userid/395421/car/304906/3517892/note.aspx\"><img id=\"_ctl0_CPH2_Pager2_Img_Pre\" title=\">>                         車検2015\" onerror=\"Minkara.NoPhoto_109(this);HidePhotosIfBothEmpty0();\" onload=\"HidePhotosIfBothEmpty0();\" src=\"http://cdn.snsimg.carview.co.jp/minkara/note/000/003/517/892/3517892/p1s.jpg?ct=d1d9f9a0bffa\" border=\"0\" /></a>";
-		//source = source.replaceAll("<[^>'\"]*['\"].*['\"].*?>", "");
-		//source = "<img src=\">\" name=\">\">";
-		source = sub_rule_03.matcher(source).replaceAll("");
+		//System.out.println(source);
+		source = sub_rule_05.matcher(source).replaceAll("\n");
+		source = sub_rule_06.matcher(source).replaceAll("\n\n\n");
+		//System.out.println(source);
 		source = sub_rule_04.matcher(source).replaceAll("");
-		//source = StringEscapeUtils.unescapeHtml(source);
+		//System.out.println(source);
+		source = StringEscapeUtils.unescapeHtml(source);
 
 		try {
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("/home/charles/Data/output/source/" + url_id + ".txt")));
@@ -105,71 +111,106 @@ public class TextExtract {
 			e.printStackTrace();
 		}
 		//System.out.println(source);
+		for(int i=0; i < blocksWidth-1; i++)
+			source = source + "\n";
 		return source;
 	
 	}
 	
 	private int calThreshold(ArrayList<Integer> data, int n){
 		if(data == null || n <=0) return -1;
-
+		if(data.size()<n) n=data.size();
 		int sum=0;
 		int len = data.size();
 		for(int i=1; i<=n; i++){
 			sum += data.get(len-i);
 		}
 		
-		return sum / n;
+		return Math.max(1,sum / (n*blocksWidth));
 	}
 	
 	private String getText() {
-		ArrayList<Integer> tmp = new ArrayList<Integer>();
-		lines = Arrays.asList(html.split("\n"));
+		ArrayList<Integer> list_for_sort = new ArrayList<Integer>();//for sort
+		lines = Arrays.asList(html.split("\n",-1));
+
+		text.setLength(0);
 		
 		int line_count=0;
+		int max_line_tokens=0;
 		int parse_max_line = (int)Math.ceil(lines.size()*main_ratio);
-		int parse_stop_line = (int)Math.ceil(lines.size()*stop_ratio);
+		int parse_stop_line=0;// = (int)Math.ceil(lines.size()*delay_ratio);
+		String[] tokens = new String[lines.size()];
+		boolean cal_flag = true;
 		//lines = lines.subList(0, parse_max_line);
 		indexDistribution.clear();
-
-		for (int i = 0; i < lines.size() - blocksWidth; i++) {
-			int wordsNum = 0;
-			for (int j = i; j < i + blocksWidth; j++) { 
-				lines.set(j, lines.get(j).replaceAll("\\s+", " ").trim());
-				wordsNum += lines.get(j).length();
+		
+		for(int i=lines.size()-1; i>(lines.size() - blocksWidth); i--){
+			tokens[i] = "";
+		}
+		try{
+			m_bw.write(m_url_id + ",");
+			for (int i = 0; i < lines.size() - blocksWidth + 1; i++) {
+//				if(cal_flag)
+//					tokens[i] = lines.get(i).replaceAll("\\s", "");
+//				if(tokens[i].length() == 0){
+//					indexDistribution.add(0);
+//					list_for_sort.add(0);
+//					cal_flag=true;
+//					continue;
+//				}
+				//if(i==83)
+				//	System.out.print(i + "\n");
+				int wordsNum = 0;
+				for (int j = i; j < i + blocksWidth; j++) { 
+					//lines.set(j, lines.get(j).trim());
+					tokens[j] = lines.get(j).replaceAll("\\s", "");
+					wordsNum += tokens[j].length();
+					//cal_flag = false;
+				}
+				
+				wordsNum = wordsNum / blocksWidth;
+				indexDistribution.add(wordsNum);
+				list_for_sort.add(wordsNum);
+				
+				//find the longest text within the main_ratio area
+				if(i < parse_max_line && list_for_sort.get(i) > max_line_tokens){
+					parse_stop_line = i;
+					max_line_tokens = list_for_sort.get(i);
+				}
+				
+				m_bw.write(wordsNum + ",");
+				//System.out.println(wordsNum);
 			}
-			wordsNum = wordsNum / blocksWidth;
-			indexDistribution.add(wordsNum);
+			//tmp = indexDistribution.clone();
 			
-			if(i < parse_max_line)
-				tmp.add(lines.get(i).length());
-			//System.out.println(wordsNum);
+			m_bw.write("\n");
+		}
+		catch(Exception e){
+			e.printStackTrace();
 		}
 		//System.out.println("lines: " + lines.size());
-		Collections.sort(tmp);
-		int avr_top_n = calThreshold(tmp, 4);
+		Collections.sort(list_for_sort);
+		//int avr_top_n = calThreshold(tmp, 3);
 
-		threshold = avr_top_n / blocksWidth;
+		//threshold = avr_top_n / blocksWidth;
+		threshold = calThreshold(list_for_sort, 3);
 		//System.out.println(threshold);
 		
 		start = -1; end = -1;
 		boolean boolstart = false, boolend = false, first_read = false;
-		text.setLength(0);
 		
 		StringBuilder buffer = new StringBuilder();
+		int line_number = indexDistribution.size();
 		try{
-		for (int i = 0; i < indexDistribution.size() - 1; i++) {
+		for (int i = 0; i < line_number ; i++) {
 			if (indexDistribution.get(i) >= threshold && ! boolstart) {
-				if (indexDistribution.get(i+1).intValue() != 0 
-					|| indexDistribution.get(i+2).intValue() != 0
-					|| indexDistribution.get(i+3).intValue() != 0) {
-					boolstart = true;
-					start = i;
-					//System.out.println("start: " + start);
-					continue;
-				}
+				boolstart = true;
+				start = i;
+				//System.out.println("start: " + start);
+				continue;
 			}
 			if (boolstart) {
-				if (indexDistribution.get(i).intValue() < 5 ) {
+				if (indexDistribution.get(i) == 0) {
 					end = i;
 					//System.out.println("end: " + end);
 					boolend = true;
@@ -182,12 +223,15 @@ public class TextExtract {
 				buffer.setLength(0);
 				//System.out.println(start+1 + "\t\t" + end+1);
 				for (int ii = start; ii < end; ii++) {
-					if (lines.get(ii).length() < 5) continue;
+					String txt = tokens[ii];
+					if (txt.length() == 0) continue;
 					if(line_count > max_lines)
 						break;
 					else
 						line_count++;
-					buffer.append(lines.get(ii) + "\n");
+					//txt = "本サービスに掲載されております電話番号情報につきましては、  利用規約をご確認ください。下記の地図はGoogleマップを使用しており正確でない場合があります。詳しくは、  利用規約をご確認ください。";
+					if(!txt.matches(".*(記事一覧|利用規約)+.*"))
+						buffer.append(txt + "\n");
 				}
 				String str = buffer.toString();
 				//System.out.println(str);
@@ -199,21 +243,23 @@ public class TextExtract {
 		}
 		}
 		catch(Exception e){
+			System.out.println(m_url_id);
 			System.out.println(e.toString());
-			//System.out.println(html);
 		}
-		
-		if (start >= end && !(first_read && (start>parse_stop_line)))
+		//for the final iteration
+		if (start > end && !(first_read && (start>parse_stop_line)))
 		{
 			buffer.setLength(0);
 			int size_1 = lines.size()-1;
 			for (int ii = start; ii <= size_1; ii++) {
-				if (lines.get(ii).length() < 5) continue;
+				String txt = tokens[ii];
+				if (txt.length() == 0) continue;
 				if(line_count > max_lines)
 					break;
 				else
 					line_count++;
-				buffer.append(lines.get(ii) + "\n");
+				if(!txt.matches(".*(記事一覧|利用規約)+.*"))
+					buffer.append(txt + "\n");
 			}
 			String str = buffer.toString();
 			//System.out.println(str);
@@ -223,6 +269,8 @@ public class TextExtract {
 			}
 		}
 		
+		//if(text.length() == 0)
+		//	return lines.get(parse_stop_line);
 		return text.toString();
 	}
 /*	
